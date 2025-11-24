f62b585e140f4efb548a7970e8b368ebbfd0ae42
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-11-24 15:54:56 +0500

    fix

--- config/conf/corsheaders.py
+++ config/conf/corsheaders.py
@@ -1,19 +1,16 @@
-# CORS_ALLOWED_ORIGINS = [
-#     "https://example.com",
-#     "http://localhost:3000",
-# ]
-
 CORS_ALLOW_ALL_ORIGINS = True
-
 CORS_ALLOW_CREDENTIALS = True
-
-CSRF_TRUSTED_ORIGINS = [
-    'https://api.meridynpharma.com/',
-]
 
 CORS_ALLOW_METHODS = [
     "DELETE",
     "GET",
+    "OPTIONS",
+    "PATCH",
     "POST",
     "PUT",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://api.meridynpharma.com',
+    'https://meridynpharma.com',
 ]
