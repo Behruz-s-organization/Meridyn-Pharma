a6c2bcfe166abde292a5f24c8a87f35e7ff25895
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-11-29 15:02:57 +0500

    tuman va place nomi uniquedan olib tashlandi

--- core/apps/shared/models/district.py
+++ core/apps/shared/models/district.py
@@ -7,7 +7,7 @@ from core.apps.accounts.models import User
 
 
 class District(BaseModel):
-    name = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='districts', null=True
     )
--- core/apps/shared/models/place.py
+++ core/apps/shared/models/place.py
@@ -10,7 +10,7 @@ class Place(BaseModel):
     district = models.ForeignKey(District, on_delete=models.CASCADE, related_name='places')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='places')
 
-    name = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
     longitude = models.FloatField(default=0.00)
     latitude = models.FloatField(default=0.00)
     extra_location = models.JSONField()
