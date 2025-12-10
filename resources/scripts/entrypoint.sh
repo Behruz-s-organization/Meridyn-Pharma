445ae78095ab26794113bc7817d8e17708a31761
behruz <xoliqberdiyevbehruz12@gmail.com>
2025-12-10 17:53:23 +0500

    core: bot qoshildi

--- resources/scripts/entrypoint.sh
+++ resources/scripts/entrypoint.sh
@@ -2,6 +2,10 @@
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 
-uvicorn config.asgi:application --host 0.0.0.0 --port 8000 --reload --reload-dir core --reload-dir config
+uvicorn config.asgi:application --host 0.0.0.0 --port 8000 --reload --reload-dir core --reload-dir config &
+
+python3 core/bot/main.py &
+
+wait
 
 exit $?
