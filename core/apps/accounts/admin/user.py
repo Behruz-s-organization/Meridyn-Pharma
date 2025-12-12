917ae210411fd3d3b6a08f042c7858fc08ef0158
xoliqberdiyev <xoliqberdiyevbehruz12@gmail.com>
2025-12-12 21:28:46 +0500

    accounts: change user add form

--- core/apps/accounts/admin/user.py
+++ core/apps/accounts/admin/user.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
 # accounts
 from core.apps.accounts.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(DjangoUserAdmin):
     list_display = ['telegram_id', 'first_name', 'last_name', 'is_active']
     search_fields = ['telegram_id', 'first_name', 'last_name']
-
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "region", "telegram_id"),
+            },
+        ),
+    )
 
 admin.site.unregister(Group)
