c76235b6591d2903f58a1905693b46012e13ac32
xoliqberdiyev <164762014+xoliqberdiyev@users.noreply.github.com>
2026-03-20 22:56:15 +0500

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,15 +1,1 @@
-{ pkgs ? import <nixpkgs> {} }:
 
-pkgs.mkShell {
-  name = "python-dev-shell";
-
-  packages = with pkgs; [
-    python312
-    python312Packages.pip
-    pyright
-  ];
-
-  shellHook = ''
-    echo "🐍 Python dev shell ready!"
-  '';
-}
